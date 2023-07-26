f0b57e036a842a10dc28e3048658f417f5f35c1a
yingkosmos <88974815+yingkosmos@users.noreply.github.com>
2023-07-26 22:42:36 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# real estate form for dataroom creation
---
+++ RE_cdrom.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Real Estate form for creation of dataroom')
+
+development_name = st.text_input('Development name', key='development_name')
+
+st.write('---------')
+st.file_uploader('Upload list of purchasers/recipients', type=['csv','xlsx','xlsm'], key='recipient_list')
+st.write('Excel file should follow the following format:')
+st.markdown('_Purchaser Name | Email | Contact Number | Postal Address | Unit Number (of development)_')
+
+st.write('---------')
+st.file_uploader('Upload documents to be shared', accept_multiple_files=True, key=f'shared_documents')
+
+def clear_form():
+    for key in st.session_state.keys():
+        del st.session_state[key]
+
+if st.button('Clear form'):
+    clear_form()
